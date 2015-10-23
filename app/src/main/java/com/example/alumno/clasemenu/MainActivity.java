efd941e890d1327e405f8c4371390e58a8ca92ac
unknown <alumno@LAB02PC17.labs.extension>
2015-10-22 21:04:21 -0300

    Primer Commit

---
+++ app/src/main/java/com/example/alumno/clasemenu/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.alumno.clasemenu;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("2048");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/2048-master/index.html");
+       // WebSettings webSettings =
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        SearchView sv = (SearchView) menu.findItem(R.id.campo_buscar).getActionView();
+        sv.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast t = Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG);
+            t.show();
+            return true;
+        }
+        else if (android.R.id.home == id){
+            this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        Log.d("Query", s);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        Log.d("Query", s);
+        return false;
+    }
+}
